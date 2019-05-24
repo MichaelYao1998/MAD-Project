1a69d5909101daa807e9c64bd1c8262f9788a724
YB1010 <37540709+YB1010@users.noreply.github.com>
2019-05-24 11:48:47 +1000

    a

--- app/src/main/java/com/e/assignment/view/MapsActivity.java
+++ app/src/main/java/com/e/assignment/view/MapsActivity.java
@@ -43,7 +43,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         EventsModel em = EventsModelImpl.getSingletonInstance(getApplicationContext());
-        em.sortTheEventList(false)
+        em.sortTheEventList(false);
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
